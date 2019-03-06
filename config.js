1ac059614a4a95fa0b66a9cb73afc8e11ed0d003
thirawutp <thirawut.p@appman.co.th>
2019-03-06 16:32:49 +0700

    rescure my code

--- config.js
+++ config.js
@@ -1,12 +1,17 @@
 const FEATURE = {
-  promo_code: false,
-  tax: true,
-  service_charge: false,
-  total: false
-};
+  "promo_code": false,
+  "tax": true,
+  "service_charge": false,
+  "total": false,
+  "tax_amount": 0.07,
+  "global_discount": true,
+}
 
 module.exports = {
   isActive: function(name) {
-    return FEATURE[name];
+    return FEATURE[name]
+  },
+  getConfig: function(name) {
+    return FEATURE[name]
   }
-};
+}
