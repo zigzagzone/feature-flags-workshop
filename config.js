beada35aa54498fd82197699d58fdceb6aa3b7c4
ariya <ariya.k@pttdigital.com>
2019-03-19 10:58:52 +0700

    add config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js 
+const FEATURE = {
+    promo_code: false,
+    tax: false,
+    service_charge: false,
+    total: false
+}
+
+module.exports = {
+    isActive: function(name) {
+        return FEATURE[name]
+    }
+}
